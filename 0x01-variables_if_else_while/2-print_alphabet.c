9b9c6c29a4eecbd5d19edab0ea9e41f28de8d90f
maurice ihiechukwu caleb <morriscaleb34@gmail.com>
2022-04-08 06:34:36 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,20 +7,15 @@
  */
 int main(void)
 {
-	int n;
-	int last;
+	char c = 'a';
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	last = n % 10;
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
 
-	if (last > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, last);
-	else if (last == 0)
-		printf("Last digit of %i is %i and is 0\n", n, last);
-	else if (last < 6)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
-
+	putchar('\n');
 	return (0);
 }
 
